901f55ee6cffbd7091dcd3faeff21223e9425c96
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-14 22:16:48 +0800

    add my info in app js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Body from './components/Body';
 import Footer from './components/Footer';
 
 const App = () => {
+  const myInfo = {
+    name: 'Salsa',
+    address: 'Banjarmasin',
+    status: 'Belum Menikah',
+    age: '25'
+  }
   return (
     <div>
       <Header />
       <Body />
+      <h2>Nama saya adalah {myInfo.name}</h2>
+      <h2>Alamat saya adalah {myInfo.address}</h2>
+      <h2>Status saya adalah {myInfo.status}</h2>
+      <h2>Umur saya adalah {myInfo.age}</h2>
       <Footer />
     </div>
   )
