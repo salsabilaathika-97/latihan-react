732887f186dffd75202af241bb0972d643aafbbe
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-14 21:49:18 +0800

    add components

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+const Body = () => {
+    return  (
+        <div>
+            <h2>Ini Body</h2>
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return  (
+        <div>
+            <h1>Ini Header</h1>
+        </div>
+    );
+};
+
+export default Header;
