7339ee5e31382a5a6e21200b2fb7ec3796923a67
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-14 23:01:00 +0800

    add call array of objects in app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 
 const App = () => {
-  const cars = ['Ayla', 'Agya', 'Sigra', 'Cayla'];
+
+  const data = [
+    {
+      id: 1,
+      email: "george.bluth@reqres.in",
+      first_name: "George",
+      last_name: "Bluth",
+      avatar: "https://reqres.in/img/faces/1-image.jpg",
+    },
+    {
+      id: 2,
+      email: "janet.weaver@reqres.in",
+      first_name: "Janet",
+      last_name: "Weaver",
+      avatar: "https://reqres.in/img/faces/2-image.jpg",
+    },
+    {
+      id: 3,
+      email: "emma.wong@reqres.in",
+      first_name: "Emma",
+      last_name: "Wong",
+      avatar: "https://reqres.in/img/faces/3-image.jpg",
+    },
+  ];
+
 
   return (
     <div>
-        <h1>Keluarga Astra</h1>
         {
-          cars.map(item => 
-            <h1>{item}</h1>
+          data.map(item => 
+            <><h1>{item.first_name} <span> {item.last_name}</span> </h1><img src={item.avatar} /></> 
           )
         }
     </div>
